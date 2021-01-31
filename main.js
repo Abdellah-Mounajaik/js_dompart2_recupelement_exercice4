4378c6ec339c48bbc80c15f333582044f3cada7b
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 19:45:12 +0100

    exo_fini

---
+++ main.js
@@ -0,0 +1,14 @@
+let recuperer = document.getElementsByClassName("redPurple")
+console.log(recuperer);
+
+let recuperer2 = document.querySelectorAll(".redPurple")[0].innerText
+console.log(recuperer2);
+
+let recuperer3 = document.querySelectorAll(".redPurple", "h1")
+console.log(recuperer3);
+
+let recuperer4 = document.querySelectorAll("li", "p","span")
+console.log(recuperer4);
+
+let recuperer5 = document.querySelectorAll("li.important", )
+console.log(recuperer5);
